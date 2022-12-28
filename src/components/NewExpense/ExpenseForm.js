455c56931cbe035da3678937f18515d3f5aa6088
oobinss <ohwon99@gmail.com>
2022-12-28 20:55:22 +0900

    동적 리스트 렌더링

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -6,21 +6,9 @@ const ExpenseForm = (props) => {
   const [enteredTitle, setEnteredTitle] = useState('')
   const [enteredAmount, setEnteredAmount] = useState('')
   const [enteredDate, setEnteredDate] = useState('')
-  // const [userInput, setUserInput] = useState({
-  //   enteredTitle : '',
-  //   enteredAmount: '',
-  //   enteredDate: '',
-  // })
 
   const titleChangeHandler = (event) => {
     setEnteredTitle(event.target.value)
-    // setUserInput({
-    //   ...userInput,
-    //   enteredTitle: event.target.value
-    // })
-    // setUserInput((prevState)=>{
-    //   return {...prevState,    enteredTitle: event.target.value};
-    // })
   }
 
   const amountChangeHandler = (event) => {
@@ -79,6 +67,9 @@ const ExpenseForm = (props) => {
         </div>
       </div>
       <div className='new-expense__actions'>
+        <button type='button' onClick={props.onCancel}>
+          Cancel
+        </button>
         <button type='submit'>Add Expense</button>
       </div>
     </form>
