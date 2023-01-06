6205e841d06f31251428f90c97653b77710a9d7c
oobinss <ohwon99@gmail.com>
2023-01-06 19:02:42 +0900

    add delete todolist

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,7 +1,32 @@
-import './App.css'
+import { useState } from 'react'
+
+import NewTodo from './components/NewTodo'
+import Todos from './components/Todos'
+import Todo from './models/todo'
 
 function App() {
-  return <div></div>
+  const [todos, setTodos] = useState<Todo[]>([])
+
+  const addTodoHandler = (todoText: string) => {
+    const newTodo = new Todo(todoText)
+
+    setTodos((prevTodos) => {
+      return prevTodos.concat(newTodo)
+    })
+  }
+
+  const removeTodoHandler = (todoId: string) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((todo) => todo.id !== todoId)
+    })
+  }
+
+  return (
+    <div>
+      <NewTodo onAddTodo={addTodoHandler} />
+      <Todos items={todos} onRemoveTodo={removeTodoHandler} />
+    </div>
+  )
 }
 
 export default App
