0adf5f1a75f7e505d4587082243c7d5b11c204cb
oobinss <ohwon99@gmail.com>
2023-01-14 13:28:30 +0900

    새로운 폼에 커스텀 훅 추가

--- working-with-forms/src/components/SimpleInput.js
+++ working-with-forms/src/components/SimpleInput.js
@@ -34,7 +34,6 @@ const SimpleInput = (props) => {
 
     console.log(enteredName)
 
-    // nameInputRef.current.value = ''; => NOT IDEAL, DON'T MANIPULATE THE DOM
     resetNameInput()
     resetEmailInput()
   }
